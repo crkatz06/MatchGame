e0afdc59be56207c70129fd63c00f5c131564207
Christopher Katz <crkatz06@email.phoenix.edu>
2021-04-26 17:37:03 -0500

    Added Timer with start and reset values, finished game.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    using System.Windows.Threading;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
+            SetUpGame();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10F).ToString("0.0s");
+
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Play Again?";
+            }
+        }
+
+        private void SetUpGame()    // SetUpGame method.
+        {
+            List<string> animalEmoji = new List<string>()   // Create a list of 8 pairs.
+            {
+                "🐙", "🐙",
+                "🐡", "🐡",
+                "🐘", "🐘",
+                "🐳", "🐳",
+                "🐫", "🐫",
+                "🦕", "🦕",
+                "🦘", "🦘",
+                "🦔", "🦔",
+
+            };
+            Random random = new Random();   // Create a new random number generator.
+
+    //  Find every TextBlock in the mainGrid and repeat the following statements for each.
+            foreach(TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                if (textBlock.Name != "timeTextBlock")  // Doesn't include Text Block "timeTextBlock".
+                {
+                    //Makes all Emoji's visible.
+                    textBlock.Visibility = Visibility.Visible;
+
+                    // Pick a random number between 0 & number of emoji left int the list, call it index.
+                    int index = random.Next(animalEmoji.Count);
+
+                    // Use the random number called "index" to get a random emoji from the list.
+                    string nextEmoji = animalEmoji[index];
+
+                    // Update the TextBlock with the random emoji from the list.
+                    textBlock.Text = nextEmoji;
+
+                    // Remove the random emoji from the list.
+                    animalEmoji.RemoveAt(index);
+                }
+            }
+            // Next 3 lines starts the timer and resets the fields.
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
+        }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+        }
+
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+                SetUpGame();
+        }
+    }
+}
